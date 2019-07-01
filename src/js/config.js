00b3088e424a0be340e537b2954a075a52ee6132
ineza <inezairene30@gmail.com>
2019-07-01 10:58:18 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+// const proxy = 'https://cors-anywhere.herokuapp.com/'; while u are not accessing api u can use this proxy to access it anywhere!!
+export const key = '5601864018c32853e8b67be620fa481e';
